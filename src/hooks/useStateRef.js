5f53d62ccb5aaf368cb46295456a9066d352cdcd
victa-desktop <24532111+dustbringer@users.noreply.github.com>
2022-01-11 03:48:18 +0000

    Added update to ref immediately on state change

--- src/hooks/useStateRef.js
+++ src/hooks/useStateRef.js
@@ -1,13 +1,20 @@
 import React, { useState, useRef, useEffect } from "react";
 
 function useStateRef(initialValue) {
-  const [value, setValue] = useState(initialValue);
+  const [value, _setValue] = useState(initialValue);
 
   const ref = useRef(value);
 
-  useEffect(() => {
-    ref.current = value;
-  }, [value]);
+  const setValue = (e) => {
+    _setValue(e);
+
+    if (typeof e === "function") ref.current = e(ref.current);
+    else ref.current = e;
+  };
+  
+  // useEffect(() => {
+  //   ref.current = value;
+  // }, [value]);
 
   return [value, setValue, ref];
 }
