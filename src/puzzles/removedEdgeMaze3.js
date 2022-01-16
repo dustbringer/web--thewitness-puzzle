996726cf55947b892ce15a900e523a2200efe670
victa-desktop <24532111+dustbringer@users.noreply.github.com>
2022-01-16 06:03:43 +0000

    Added missing link

--- src/puzzles/removedEdgeMaze3.js
+++ src/puzzles/removedEdgeMaze3.js
@@ -1,6 +1,6 @@
 import Puzzle from "../classes/Puzzle";
 
-// https://static.wikia.nocookie.net/thewitness_gamepedia/images/1/12/Entry_Area_2.jpg/revision/latest?cb=20160325114418
+// https://static.wikia.nocookie.net/thewitness_gamepedia/images/6/65/Entry_Area_3.jpg/revision/latest?cb=20160325114429
 export default function create() {
   const p = new Puzzle(10, 10);
   p.addStart(0, 10);
