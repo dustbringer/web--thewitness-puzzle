33d047e108bf213f4a9a417c4e86ecdc67b9cf37
victa-desktop <24532111+dustbringer@users.noreply.github.com>
2022-01-16 07:36:42 +0000

    Added routing and create puzzle page

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,21 @@
 import * as React from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Container from "./components/Container";
-import Puzzle from "./components/Puzzle";
-
-import PuzzleClass from "./classes/Puzzle";
-import { VtxSym, SpcSym, EdgSym } from "./enums/Sym";
-import Orientation from "./enums/Orientation";
-
-import removedEdgeMaze1 from "./puzzles/removedEdgeMaze1";
-import removedEdgeMaze2 from "./puzzles/removedEdgeMaze2";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Create from "./pages/Create";
 
 function App() {
-  let p = new PuzzleClass(4, 4);
-  p.addStart(8, 4);
-  p.addStart(4, 4);
-  p.addStart(0, 8);
-  p.addStart(0, 0);
-  p.addStart(6, 7);
-  p.addStart(1, 6);
-  p.addStart(7, 8);
-  p.addEnd(0, 2);
-  p.addEnd(0, 3);
-  p.addEnd(0, 4);
-
-  // p.addEdgSym(0, 1, EdgSym.BREAK);
-  // p.addEdgSym(1, 0, EdgSym.BREAK);
-  // p.addEdgSym(2, 1, EdgSym.BREAK);
-  // p.addEdgSym(1, 2, EdgSym.BREAK);
-  // p.addEdgSym(2, 3, EdgSym.BREAK);
-  // p.addEdgSym(3, 2, EdgSym.BREAK);
-  p.addVtxSym(0, 2, VtxSym.DOT);
-  p.addSpcSym(1, 1, SpcSym.SUN);
-
-  console.log(p.grid);
-
   return (
-    <div>
-      <Container maxWidth="md">
-        asdasjkdnakjsndsad
-        <Puzzle puzzle={p} />
-      </Container>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="create" element={<Create />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
